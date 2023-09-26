2ee6f822e52c15ab9913d5eaa82eb13081f5de67
hungpham1406 <hung.pham14062003@hcmut.edu.vn>
2023-09-26 10:28:05 +0700

    Update timer function for ex 1

---
+++ Source_Code/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 26, 2023
+ *      Author: Asus
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
