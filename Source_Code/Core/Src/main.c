e9852a804bcc163924b0f04a8c4185f98cbf61bc
hungpham1406 <hung.pham14062003@hcmut.edu.vn>
2023-09-27 09:35:06 +0700

    Complete exercise 1

--- Source_Code/Core/Src/main.c
+++ Source_Code/Core/Src/main.c
@@ -182,7 +182,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -194,7 +194,6 @@ int main(void)
 	  if(timer1_flag == 1) {
 		  setTimer1(50);
 
-		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, SET);
 		  if(state == 0) {
 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
