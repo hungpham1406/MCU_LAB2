6b29274cd8b8b345717982f96eaf2e3216e4411a
hungpham1406 <hung.pham14062003@hcmut.edu.vn>
2023-10-02 12:13:51 +0700

    Update exercise 3

--- Source_Code/Core/Src/main.c
+++ Source_Code/Core/Src/main.c
@@ -162,28 +162,28 @@ void update7SEG(int index) {
 		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-		  display7SEG(led_buffer[0]);
+		  display7SEG(led_buffer[index]);
 		  break;
 	  case 1:
 		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
 		  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-		  display7SEG(led_buffer[1]);
+		  display7SEG(led_buffer[index]);
 		  break;
 	  case 2:
 		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
 		  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-		  display7SEG(led_buffer[2]);
+		  display7SEG(led_buffer[index]);
 		  break;
 	  case 3:
 		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
-		  display7SEG(led_buffer[3]);
+		  display7SEG(led_buffer[index]);
 		  break;
 	  default:
 		  break;
